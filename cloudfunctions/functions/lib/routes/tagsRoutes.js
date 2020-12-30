aac81088bd575174dea4982387cf4faafce4a808
axelkhellberg <axelkhellberg@gmail.com>
2020-12-30 09:35:21 -0300

    Se agrego el servicio deshabilitarTagSeleccionado

--- cloudfunctions/functions/lib/routes/tagsRoutes.js
+++ cloudfunctions/functions/lib/routes/tagsRoutes.js
@@ -1,14 +1,35 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const genericRoutes_1 = require("./genericRoutes");
 const TagService_1 = require("../services/TagService");
 const Tag_1 = require("../entity/Tag");
+const typeorm_1 = require("typeorm");
+const global_1 = require("../global");
+const apiHandler_1 = require("../components/apiHandler");
 var express = require('express');
 var router = express.Router();
 /******************************************** */
 const service = new TagService_1.TagService();
 const currentClass = Tag_1.Tag;
 /******************************************** */
-router = genericRoutes_1.addToGenericRoute(router, currentClass, service);
+router.post('/deshabilitarTagSeleccionado', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        let r = yield typeorm_1.getConnection().query("UPDATE " + global_1.GlobalVariable.DATA_BASE_NAME + ".tag SET asignado = 1 WHERE id=" + req.body.id);
+        console.log("res");
+        console.log(r);
+        res.status(200).send(r);
+    }
+    catch (e) {
+        yield apiHandler_1.responseError(res, e);
+    }
+}));
 module.exports = router;
 //# sourceMappingURL=tagsRoutes.js.map
--- cloudfunctions/functions/src/routes/tagsRoutes.ts
+++ cloudfunctions/functions/src/routes/tagsRoutes.ts
@@ -1,6 +1,9 @@
 import { addToGenericRoute } from "./genericRoutes";
 import { TagService } from "../services/TagService";
 import { Tag } from "../entity/Tag";
+import { getConnection } from "typeorm";
+import { GlobalVariable } from "../global";
+import { responseError } from "../components/apiHandler";
 var express = require('express');
 var router = express.Router();
 
@@ -9,6 +12,19 @@ const service = new TagService()
 const currentClass = Tag
 /******************************************** */
 
-router = addToGenericRoute(router, currentClass, service)
+router.post('/deshabilitarTagSeleccionado', async (req, res, next) => {
+
+    try {
+        let r = await getConnection().query("UPDATE "+ GlobalVariable.DATA_BASE_NAME +".tag SET asignado = 1 WHERE id="+ req.body.id);
+
+        console.log("res");
+        console.log(r);
+        res.status(200).send(r);
+        
+    } catch (e) {
+        await responseError(res, e)
+
+    }
+})
 
 module.exports = router;
