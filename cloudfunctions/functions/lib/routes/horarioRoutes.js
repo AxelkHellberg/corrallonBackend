39d2938901f581dd0803c6313d04c30f3ae70f2d
axelkhellberg <axelkhellberg@gmail.com>
2021-01-08 15:21:11 -0300

    servicio nuevo en horarioRoutes.ts

--- cloudfunctions/functions/lib/routes/horarioRoutes.js
+++ cloudfunctions/functions/lib/routes/horarioRoutes.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require('express');
 var router = express.Router();
@@ -6,10 +15,24 @@ const genericRoutes_1 = require("./genericRoutes");
 /******************************************************************************* */
 const Horario_1 = require("../entity/Horario");
 const HorarioService_1 = require("../services/HorarioService");
+const typeorm_1 = require("typeorm");
+const global_1 = require("../global");
+const apiHandler_1 = require("../components/apiHandler");
 /******************************************** */
 const service = new HorarioService_1.HorarioService();
 const currentClass = Horario_1.Horario;
 /******************************************** */
 router = genericRoutes_1.addToGenericRoute(router, currentClass, service);
+router.post('/traerRondasHoyPorUsuario', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        let r = yield typeorm_1.getConnection().query("SELECT h.horaInicio ,h.horaFin ,u.username, pr.nombre,h.fechaInicio as fecha FROM " + global_1.GlobalVariable.DATA_BASE_NAME + ".horario h INNER JOIN " + global_1.GlobalVariable.DATA_BASE_NAME + ".horario_persona hp ON h.id = hp.horarioId INNER JOIN " + global_1.GlobalVariable.DATA_BASE_NAME + ".`user` u ON u.id = hp.userId INNER JOIN " + global_1.GlobalVariable.DATA_BASE_NAME + ".plantilla_ronda pr ON h.plantillaId = pr.id WHERE h.fechaInicio = CURDATE() and u.username ='" + req.body.username + "'");
+        console.log("res");
+        console.log(r);
+        res.status(200).send(r);
+    }
+    catch (e) {
+        yield apiHandler_1.responseError(res, e);
+    }
+}));
 module.exports = router;
 //# sourceMappingURL=horarioRoutes.js.map
--- cloudfunctions/functions/src/routes/horarioRoutes.ts
+++ cloudfunctions/functions/src/routes/horarioRoutes.ts
@@ -4,6 +4,9 @@ import { addToGenericRoute } from "./genericRoutes";
 /******************************************************************************* */
 import { Horario } from "../entity/Horario";
 import { HorarioService } from "../services/HorarioService";
+import { getConnection } from "typeorm";
+import { GlobalVariable } from "../global";
+import { responseError } from "../components/apiHandler";
 
 /******************************************** */
 const service = new HorarioService()
@@ -11,5 +14,20 @@ const currentClass = Horario
 /******************************************** */
 
 router = addToGenericRoute(router, currentClass, service)
+
+router.post('/traerRondasHoyPorUsuario', async (req, res, next) => {
+
+    try {
+        let r = await getConnection().query("SELECT h.horaInicio ,h.horaFin ,u.username, pr.nombre,h.fechaInicio as fecha FROM "+ GlobalVariable.DATA_BASE_NAME +".horario h INNER JOIN "+ GlobalVariable.DATA_BASE_NAME +".horario_persona hp ON h.id = hp.horarioId INNER JOIN "+ GlobalVariable.DATA_BASE_NAME +".`user` u ON u.id = hp.userId INNER JOIN "+ GlobalVariable.DATA_BASE_NAME +".plantilla_ronda pr ON h.plantillaId = pr.id WHERE h.fechaInicio = CURDATE() and u.username ='"+ req.body.username+"'");
+
+        console.log("res");
+        console.log(r);
+        res.status(200).send(r);
+        
+    } catch (e) {
+        await responseError(res, e)
+
+    }
+})
 
 module.exports = router;
